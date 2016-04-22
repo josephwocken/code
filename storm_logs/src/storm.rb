7c2a68b9fb00dbeea1c088515aa3bf9f7f685c1f
josephwocken <josephwocken@gmail.com>
2016-04-21 20:24:35 -0500

    initial commit

---
+++ storm_logs/src/storm.rb
@@ -0,0 +1,40 @@
+require 'json'
+require 'rest-client'
+require 'time'
+
+
+rest_arr = Array.new
+entry_arr = Array.new
+
+File.open("servers.txt", "r") do |file|
+  file.each_line do |line|
+    rest_arr.push(line)
+  end
+end
+
+rest_arr.each do |url|
+  puts url
+end
+
+#loop begin
+#response = RestClient.get 'XXXX'
+#add response to array of hashes
+#loop end
+
+#loop begin (response array of hashes)
+## parse response
+log_entry = ""
+t = Time.new
+log_entry << t.iso8601
+puts log_entry
+## add "topology", "server", "status", "timestamp" (something like string builder in java)
+## push to final array of strings
+entry_arr.push(log_entry)
+#loop end
+
+#loop begin
+## write each string index (reprenting one log entry) to a file (same one that logstash is watching)
+entry_arr.each do |log|
+  #write to file
+end
+#loop end
